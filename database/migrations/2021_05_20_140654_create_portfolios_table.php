feb90378af31cf0baad84ad97e04f5e99445fdfd
iotronlab <iotronlabspvt@gmail.com>
2021-05-20 20:05:48 +0530

    portfolio

---
+++ database/migrations/2021_05_20_140654_create_portfolios_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('client_brief');
+            $table->string('project_description');
+            $table->json('tools');
+            $table->json('images');
+            $table->text('typography');
+            $table->text('palette');
+            $table->string('image');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
---
+++ database/migrations/2021_05_20_141152_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('name');
+            $table->string('content');
+            $table->string('portfolio_id');
+            $table->json('images');
+            $table->timestamps();
+
+            $table->foreign('portfolio_id')->references('id')->on('portfolios');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
