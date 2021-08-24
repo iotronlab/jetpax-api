57727f02138285f1bfd9087a65ad1fbb8b43a1f4
iotronlab <iotronlabspvt@gmail.com>
2021-08-24 21:22:55 +0530

    enlightn error fix

--- app/Http/Controllers/api/CreatorController.php
+++ app/Http/Controllers/api/CreatorController.php
@@ -11,14 +11,12 @@ use App\Scoping\Scopes\CategoriesScope;
 use App\Scoping\Scopes\FollowersScope;
 use App\Scoping\Scopes\LanguagesScope;
 use App\Scoping\Scopes\SocialScope;
-use Illuminate\Http\Request;
 
 class CreatorController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -40,32 +38,10 @@ class CreatorController extends Controller
         ];
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Creator  $creator
-     * @return \Illuminate\Http\Response
      */
     public function show(Creator $creator)
     {
@@ -73,39 +49,5 @@ class CreatorController extends Controller
         return new CreatorShowResource(
             $creator
         );
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Creator  $creator
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Creator $creator)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Creator  $creator
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Creator $creator)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Creator  $creator
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Creator $creator)
-    {
-        //
     }
 }
